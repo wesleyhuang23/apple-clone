9c103ec7965a901da086a0550e8e70344bcdf07a
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-04-20 15:51:04 -0700

    macbook pro chapter with styling

--- src/components/Mac/Mac.vue
+++ src/components/Mac/Mac.vue
@@ -1,14 +1,19 @@
 <template>
-<chapterNav></chapterNav>
+    <div>
+        <chapterNav></chapterNav>
+        <macBookProChapter></macBookProChapter>
+    </div>
 </template>
 
 <script>
 import chapterNav from './MacChapterNav';
+import macBookProChapter from './MacBookProChapter';
 
 export default{
     name: 'Mac',
     components: {
         chapterNav,
+        macBookProChapter
     },
 }
 </script>
---
+++ src/components/Mac/MacBookProChapter.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="mbp-chapter">
+        <div class="header">
+            <h2>Macbook Pro</h2>
+            <h1>A touch of genius.</h1>
+            <p><span>Learn more ></span><span>Buy ></span></p>
+        </div>
+        <div class="img">
+            <img src="./../../assets/apple-img/mac/macbookpro_large_2x.png"/> 
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+    .mbp-chapter{
+        height: 80vh;
+        overflow: hidden;
+        position: relative;
+        display: flex;
+        justify-content: center;
+    }
+    .header{
+        text-align: center;
+        padding-top: 100px;
+
+        h1{
+            font-size:55px;
+            font-weight: 500;
+            margin-bottom: 20px;
+        }
+        span{
+            padding-right: 20px;
+            font-size: 20px;
+            font-weight: lighter;
+            color: #0070C9;
+        }
+    }
+    .img{
+        position: absolute;
+        bottom: 0px;
+        width: 175%;
+
+        img{
+            width: 100%;
+        }
+    }
+</style>
