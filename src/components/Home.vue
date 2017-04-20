77fc667956b96d2041bec6d9e36c822597ed161c
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-04-19 17:36:01 -0700

    footer nav with position styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,40 +1,133 @@
 <template>
   <div class="home">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-      </ol>
+    <div class="carousel">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+          <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active" id="red">
-          <img src="./../assets/apple-img/home/iphone7_productred_medium_2x.png" alt="Chania">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active" id="red">
+            <img src="./../assets/apple-img/home/iphone7_productred_medium_2x.png" alt="Chania">
+          </div>
+
+          <div class="item" id="w">
+            <img src="./../assets/apple-img/home/watch_medium_2x.jpg" alt="Chania">
+          </div>
+
+          <div class="item" id="pad">
+            <img src="./../assets/apple-img/home/ipad_availability_medium_2x.jpg" alt="Flower">
+          </div>
+
         </div>
 
-        <div class="item" id="w">
-          <img src="./../assets/apple-img/home/watch_medium_2x.jpg" alt="Chania">
-        </div>
-
-        <div class="item" id="pad">
-          <img src="./../assets/apple-img/home/ipad_availability_medium_2x.jpg" alt="Flower">
-        </div>
-
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
-
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
+
+    <footer>
+      <div class="more">
+        <div class="shop-and-learn"> 
+          <ul>
+            <li>Shop and Learn</li>
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>TV</li>
+            <li>Music</li>
+            <li>iTunes</li>
+            <li>iPod</li>
+            <li>Accessories</li>
+            <li>Gift Cards</li>
+          </ul>
+        </div>
+
+        <div class="apple-store">
+          <ul>
+            <li>Apple Store</li>
+            <li>Find a Store</li>
+            <li>Genius Bar</li>
+            <li>Workshops and Learning</li>
+            <li>Youth Programs</li>
+            <li>Apple Store App</li>
+            <li>Refurbished and Clearance</li>
+            <li>Financing</li>
+            <li>Reuse and Recycling</li>
+            <li>Order Status</li>
+            <li>Shopping Help</li>
+          </ul>
+        </div>
+
+        <div class="b+e">
+
+          <div class="for-education">
+            <ul>
+              <li>For Education</li>
+              <li>Apple and Education</li>
+              <li>Shop for Change</li>
+            </ul>
+          </div>
+          <div class="for-business"> 
+            <ul>
+              <li>For Business</li>
+              <li>Apple and Business</li>
+              <li>Shop for Business</li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div class="a+a">
+
+          <div class="account">
+            <ul>
+              <li>Account</li>
+              <li>Manage Your Apple ID</li>
+              <li>Apple Store Account</li>
+              <li>iCloud.com</li>
+            </ul>
+          </div>
+          <div class="apple-values">
+            <ul>
+              <li>Apples Values</li>
+              <li>Accessibility</li>
+              <li>Education</li>
+              <li>Environment</li>
+              <li>Inclusion and Diversity</li>
+              <li>Privacy</li>
+              <li>Supplier Responsibility</li>
+            </ul>
+          </div>
+        
+        </div>
+
+        <div class="about-apple"> 
+          <ul>
+              <li>About Apple</li>
+              <li>Apple Info</li>
+              <li>Newroom</li>
+              <li>Job Opportunities</li>
+              <li>Press Info</li>
+              <li>Investors</li>
+              <li>Contact Apple</li>
+            </ul>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -51,7 +144,7 @@ export default {
 
 
 <style lang="scss" scoped>
-  .home{
+  .carousel{
     height: 90vh;
     overflow: hidden;
   }
@@ -73,6 +166,31 @@ export default {
       padding-top: 300px;
       margin: auto;
       width: 1200px;
+    }
+  }
+  footer{
+    height: 30vh;
+    background-color: #F2F2F2;
+    display: flex;
+    justify-content: center;
+  }
+  .more{
+    display: flex;
+    justify-content: space-between;
+    width: 980px;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 20px;
+  }
+  ul{
+    list-style: none;
+  }
+  li{
+    font-weight: lighter;
+    font-size: 12px;
+
+    &:first-child{
+      font-weight: bold;
     }
   }
 </style>
